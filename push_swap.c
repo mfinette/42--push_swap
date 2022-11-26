faf6fd6525010c3bf03e2e0c6182d29cd57cf8a1
Maxime Finette <mfinette@z4r11p3.42lyon.fr>
2022-11-26 17:27:15 +0100

    debugging

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:47:26 by mfinette          #+#    #+#             */
-/*   Updated: 2022/11/26 14:31:49 by mfinette         ###   ########.fr       */
+/*   Updated: 2022/11/26 17:26:14 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ int	main(int argc, char **argv)
 		data = get_data_n(&argv[1], argc);
 	if (check_input(&data, argv[1]))
 	{
-		//print_tabs(&data);
-		sort_long(&data);
-		//print_tabs(&data);
+		print_tabs(&data);
+		sort_hundred(&data);
+		print_tabs(&data);
 	}
 	free_all(data);
 	return (0);
@@ -57,7 +57,7 @@ void	sort_all(t_stacks *data)
 			sa(data);
 		if (r == 5)
 			sb(data);
-		if (r == 6)		
+		if (r == 6)
 			ss(data);
 		if (r == 7)
 			rra(data);
@@ -126,7 +126,7 @@ int	sort_three(t_stacks *data)
 int	is_in_b(t_stacks *data, int tmp)
 {
 	int	i;
-	
+
 	i = 0;
 	while (i < data->size_b)
 	{
@@ -200,10 +200,147 @@ int	sort_five(t_stacks *data)
 	return (0);
 }
 
-int	sort_hunder(t_stacks *data)
+int	check_faster_to_top(t_stacks *data, int nb1, int nb2)
 {
-	int	chunks;
+	int	i;
+	int	top_dist1;
+	int	bot_dist1;
+	int	top_dist2;
+	int	bot_dist2;
 
-	chunks = data->size_sort / 5;
+	i = 0;
+	while (data->stack_a[i] != nb1)
+		i++;
+	bot_dist1 = i;
+	top_dist1 = data->size_a - i - 1;
+	i = 0;
+	while (data->stack_a[i] != nb2)
+		i++;
+	bot_dist2 = i;
+	top_dist2 = data->size_a - i - 1;
+	if (bot_dist2 < top_dist2)
+		top_dist2 = bot_dist2;
+	if (bot_dist1 < top_dist1)
+		top_dist1 = bot_dist1;
+	if (top_dist1 > top_dist2)
+		push_number_to_top(data, nb1);
+	else
+		push_number_to_top(data, nb2);
+	return (i);
+}
+
+int	is_in_chunk(t_stacks *data, int chunk, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (i <= chunk)
+	{
+		if (data->stack_sort[i] == nb)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	push_b_to_top(t_stacks *data, int nb)
+{
+	int	i;
+	int	top_dist;
+	int	bot_dist;
+
+	i = 0;
+	if (data->size_b == 0)
+		return (1);
+	while (data->stack_b[i] != nb)
+		i++;
+	bot_dist = i;
+	top_dist = data->size_b - i - 1;
+	i = 0;
+	while (data->stack_b[data->size_b - 1] != nb)
+	{
+		if (top_dist > bot_dist)
+		{
+			rrb(data);
+			i++;
+		}
+		else
+		{
+			rb(data);
+			i++;
+		}
+	}
+	return (i);
+}
+
+void	move_to_b(t_stacks *data)
+{
+	int	i;
+	int	tmp;
+
+	i = 0;
+	tmp = 0;
+	while (i < data->size_b)
+	{
+		if (data->stack_b[i] > data->stack_a[data->size_a - 1])
+		{
+			tmp = data->stack_b[i];
+		}
+		i++;
+	}
+	while (i < data->size_b)
+	{
+		if (data->stack_b[i] > data->stack_a[data->size_a - 1] && data->stack_b[i] < tmp)
+			tmp = data->stack_b[i];
+		i++;
+	}
+	printf("(MOVE_TO_B) tmp = %d\n", tmp);
+	push_b_to_top(data, tmp);
+	pb(data);
+}
+
+int	sort_hundred(t_stacks *data)
+{
+	int	i;
+	int	j;
+	int	chunk;
+	int	temp1;
+	int	temp2;
+
+	temp1 = 0;
+	j = 0;
+	i = data->size_a - 1;
+	chunk = data->size_sort / 5;
+	while (chunk <= data->size_sort)
+	{
+		while (i >= 0)
+		{
+			if (is_in_chunk(data, chunk, data->stack_a[i]))
+			{
+				temp1 = data->stack_a[i];
+				break ;
+			}
+			i--;
+		}
+		i--;
+		while (i >= 0)
+		{
+			if (is_in_chunk(data, chunk, data->stack_a[i]))
+			{
+				temp2 = data->stack_a[i];
+				break ;
+			}
+			i--;
+		}	
+		printf("temp1 = %d temp2 = %d\n", temp1, temp2);
+		check_faster_to_top(data, temp1, temp2);
+		move_to_b(data);
+		j++;
+		if (j == data->size_sort / 5)
+		{
+			j = 0;
+			chunk = chunk + (data->size_sort / 5);
+		}
+	}
 	return (0);
 }
